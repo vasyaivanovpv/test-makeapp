e8a5583145f179fbd64c2cd2e987d17c3b968c90
Вася Иванов <soldatik2005@mail.ru>
2020-06-05 16:29:01 +0300

    Updates

---
+++ precache-manifest.e34dbd0d2ae71c805da883144e24225e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853838cff04bb1427f4bee83a5add493",
+    "url": "/test-makeapp/index.html"
+  },
+  {
+    "revision": "b2c1f1a53b452d4c4eb2",
+    "url": "/test-makeapp/static/css/main.fb62e704.chunk.css"
+  },
+  {
+    "revision": "5375644f554120277bbb",
+    "url": "/test-makeapp/static/js/2.e5e16aef.chunk.js"
+  },
+  {
+    "revision": "b2c1f1a53b452d4c4eb2",
+    "url": "/test-makeapp/static/js/main.916f454c.chunk.js"
+  },
+  {
+    "revision": "89502ab848a594acd5d1",
+    "url": "/test-makeapp/static/js/runtime-main.6cb0f18e.js"
+  },
+  {
+    "revision": "473e321239aa0be55889a124daf6e0d3",
+    "url": "/test-makeapp/static/media/MuseoSansCyrl-500.473e3212.ttf"
+  },
+  {
+    "revision": "3b1501e4985e4d60dd05df6162795ee9",
+    "url": "/test-makeapp/static/media/MuseoSansCyrl-900.3b1501e4.ttf"
+  },
+  {
+    "revision": "1c77cd6d0161f490e47e84a67c84e7c2",
+    "url": "/test-makeapp/static/media/svg_android.1c77cd6d.svg"
+  },
+  {
+    "revision": "528096b969bb6cf96bcc56bc456fdac7",
+    "url": "/test-makeapp/static/media/svg_apple.528096b9.svg"
+  },
+  {
+    "revision": "189b9cffb99dcbb9afc1486eee1c0a57",
+    "url": "/test-makeapp/static/media/svg_mark.189b9cff.svg"
+  },
+  {
+    "revision": "9c2818ab1e7d05c7b7a3edf2149ddcdf",
+    "url": "/test-makeapp/static/media/svg_star.9c2818ab.svg"
+  }
+]);
